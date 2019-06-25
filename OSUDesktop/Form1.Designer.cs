3d76a915c925f860c06598559d3e36ccce4d4605
TGSAN <zhujunwei@bilibili.com>
2019-06-25 17:06:38 +0800

    Add support MUSE DASH
    Add support MUSE DASH.
    Support Steam games.

--- OSUDesktop/Form1.Designer.cs
+++ OSUDesktop/Form1.Designer.cs
@@ -34,26 +34,33 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.AsWallpaperToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ReShowOSUToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AsWallpaperToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infolabel2 = new System.Windows.Forms.Label();
+            this.infolabel1 = new System.Windows.Forms.Label();
+            this.StartCheckBtn = new System.Windows.Forms.Button();
+            this.progNameBox = new System.Windows.Forms.TextBox();
+            this.checkTimer = new System.Windows.Forms.Timer(this.components);
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // StartOSU
             // 
-            this.StartOSU.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.StartOSU.Location = new System.Drawing.Point(0, 0);
+            this.StartOSU.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StartOSU.Location = new System.Drawing.Point(12, 166);
+            this.StartOSU.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.StartOSU.Name = "StartOSU";
-            this.StartOSU.Size = new System.Drawing.Size(227, 70);
+            this.StartOSU.Size = new System.Drawing.Size(415, 51);
             this.StartOSU.TabIndex = 0;
-            this.StartOSU.Text = "启动";
+            this.StartOSU.Text = "启动进程";
             this.StartOSU.UseVisualStyleBackColor = true;
             this.StartOSU.Click += new System.EventHandler(this.StartOSU_Click);
             // 
             // openFileDialog1
             // 
-            this.openFileDialog1.Filter = "OSU!主程序|osu!.exe";
+            this.openFileDialog1.Filter = "MuseDash主程序|MuseDash.exe|OSU!主程序|osu!.exe";
             this.openFileDialog1.Title = "请选择OSU!主程序";
             this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
@@ -70,42 +77,98 @@
             this.AsWallpaperToolStripMenuItem,
             this.ExitToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 92);
-            // 
-            // ExitToolStripMenuItem
-            // 
-            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
-            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.ExitToolStripMenuItem.Text = "退出";
-            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
-            // 
-            // AsWallpaperToolStripMenuItem
-            // 
-            this.AsWallpaperToolStripMenuItem.Name = "AsWallpaperToolStripMenuItem";
-            this.AsWallpaperToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.AsWallpaperToolStripMenuItem.Text = "设置壁纸";
-            this.AsWallpaperToolStripMenuItem.Click += new System.EventHandler(this.AsWallpaperToolStripMenuItem_Click);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(131, 70);
             // 
             // ReShowOSUToolStripMenuItem
             // 
             this.ReShowOSUToolStripMenuItem.Name = "ReShowOSUToolStripMenuItem";
-            this.ReShowOSUToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ReShowOSUToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.ReShowOSUToolStripMenuItem.Text = "还原OSU!";
             this.ReShowOSUToolStripMenuItem.Click += new System.EventHandler(this.ReShowOSUToolStripMenuItem_Click);
             // 
+            // AsWallpaperToolStripMenuItem
+            // 
+            this.AsWallpaperToolStripMenuItem.Name = "AsWallpaperToolStripMenuItem";
+            this.AsWallpaperToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.AsWallpaperToolStripMenuItem.Text = "设置壁纸";
+            this.AsWallpaperToolStripMenuItem.Click += new System.EventHandler(this.AsWallpaperToolStripMenuItem_Click);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.ExitToolStripMenuItem.Text = "退出";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // infolabel2
+            // 
+            this.infolabel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.infolabel2.AutoSize = true;
+            this.infolabel2.Location = new System.Drawing.Point(12, 145);
+            this.infolabel2.Name = "infolabel2";
+            this.infolabel2.Size = new System.Drawing.Size(332, 17);
+            this.infolabel2.TabIndex = 1;
+            this.infolabel2.Text = "传统模式：通过下方按钮启动游戏（不兼容游戏平台的游戏）";
+            // 
+            // infolabel1
+            // 
+            this.infolabel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.infolabel1.Location = new System.Drawing.Point(12, 9);
+            this.infolabel1.Name = "infolabel1";
+            this.infolabel1.Size = new System.Drawing.Size(415, 39);
+            this.infolabel1.TabIndex = 2;
+            this.infolabel1.Text = "兼容模式：填入游戏进程文件名，手动启动游戏后，点击“检测”按钮开始检测并自动开始";
+            // 
+            // StartCheckBtn
+            // 
+            this.StartCheckBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StartCheckBtn.Location = new System.Drawing.Point(12, 81);
+            this.StartCheckBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.StartCheckBtn.Name = "StartCheckBtn";
+            this.StartCheckBtn.Size = new System.Drawing.Size(415, 51);
+            this.StartCheckBtn.TabIndex = 3;
+            this.StartCheckBtn.Text = "检测";
+            this.StartCheckBtn.UseVisualStyleBackColor = true;
+            this.StartCheckBtn.Click += new System.EventHandler(this.StartCheckBtn_Click);
+            // 
+            // progNameBox
+            // 
+            this.progNameBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progNameBox.Location = new System.Drawing.Point(12, 51);
+            this.progNameBox.Name = "progNameBox";
+            this.progNameBox.Size = new System.Drawing.Size(415, 23);
+            this.progNameBox.TabIndex = 4;
+            this.progNameBox.Text = "MuseDash.exe";
+            // 
+            // checkTimer
+            // 
+            this.checkTimer.Interval = 1000;
+            this.checkTimer.Tick += new System.EventHandler(this.CheckTimer_Tick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(227, 70);
+            this.ClientSize = new System.Drawing.Size(439, 230);
+            this.Controls.Add(this.progNameBox);
+            this.Controls.Add(this.StartCheckBtn);
+            this.Controls.Add(this.infolabel1);
+            this.Controls.Add(this.infolabel2);
             this.Controls.Add(this.StartOSU);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "OSU!Desktop";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -118,6 +181,11 @@
         private System.Windows.Forms.ToolStripMenuItem AsWallpaperToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ReShowOSUToolStripMenuItem;
+        private System.Windows.Forms.Label infolabel2;
+        private System.Windows.Forms.Label infolabel1;
+        private System.Windows.Forms.Button StartCheckBtn;
+        private System.Windows.Forms.TextBox progNameBox;
+        private System.Windows.Forms.Timer checkTimer;
     }
 }
 
--- OSUDesktop/Form1.cs
+++ OSUDesktop/Form1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Management;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
@@ -25,8 +28,22 @@ namespace OSUDesktop
             SMTO_ERRORONEXIT = 0x20
         }
 
+        internal const uint GW_OWNER = 4;
+
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         public static extern IntPtr FindWindow([MarshalAs(UnmanagedType.LPTStr)] string lpClassName, [MarshalAs(UnmanagedType.LPTStr)] string lpWindowName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        internal static extern int GetWindowThreadProcessId(IntPtr hWnd, out IntPtr lpdwProcessId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        internal static extern IntPtr GetWindow(IntPtr hWnd, uint uCmd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        internal static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        private static extern int SendMessage(IntPtr hWnd, int msg, uint wParam, uint lParam);
 
         [DllImport("user32")]
         private static extern IntPtr FindWindowEx(IntPtr hWnd1, IntPtr hWnd2, string lpsz1, string lpsz2);
@@ -42,9 +59,6 @@ namespace OSUDesktop
         public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
         public delegate bool EnumWindowsProc(IntPtr hwnd, IntPtr lParam);
 
-        [DllImport("User32.dll", EntryPoint = "SendMessage")]
-        private static extern int SendMessage(IntPtr hWnd, int msg, uint wParam, uint lParam);
-
         public Form1()
         {
             InitializeComponent();
@@ -52,26 +66,38 @@ namespace OSUDesktop
 
         private void StartOSU_Click(object sender, EventArgs e)
         {
-            openFileDialog1.ShowDialog();//显示选取对话框
+            openFileDialog1.ShowDialog(); // 显示选取对话框
         }
 
-        public static System.Diagnostics.Process p;//定义进程p
+        public static System.Diagnostics.Process p; // 定义进程p
+
+        StartMode cmode;
+
+        enum StartMode
+        {
+            ModeA, // 兼容模式
+            ModeB, // 传统模式
+        }
+
+        int ModeA_PID = 0;
+        string ModeA_Name = "";
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
             MessageBox.Show("请先将游戏设置为无边框窗口全屏，然后在系统托盘设置壁纸即可~");
-            p = System.Diagnostics.Process.Start(openFileDialog1.FileName);//在进程p上运行OSU!
-            Hide();//隐藏窗口
-            notifyIcon1.Visible = true;//显示托盘图标
+            cmode = StartMode.ModeB;
+            p = System.Diagnostics.Process.Start(openFileDialog1.FileName); // 在进程p上运行OSU!
+            Hide(); // 隐藏窗口
+            notifyIcon1.Visible = true; // 显示托盘图标
         }
 
         public static Boolean winseven = false;
 
-        IntPtr WorkerWIntPtr = fWorker();//定义句柄并设置为桌面WorkerW的句柄
+        IntPtr WorkerWIntPtr = fWorker(); // 定义句柄并设置为桌面WorkerW的句柄
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //系统检测
+            // 系统检测
             Version currentVersion = Environment.OSVersion.Version;
             Version compareToVersion = new Version("6.2");
             if (currentVersion.CompareTo(compareToVersion) >= 0)
@@ -84,7 +110,7 @@ namespace OSUDesktop
                 Console.WriteLine("当前系统不是WIN8及以上版本系统。");
                 winseven = true;
             }
-            //冲突检测，是否运行了Teamviewer等程序导致桌面句柄消失
+            // 冲突检测，是否运行了Teamviewer等程序导致桌面句柄消失
             if (WorkerWIntPtr.ToString() == "0")
             {
                 MessageBox.Show("检测到桌面环境异常，请检查是否正在运行冲突的程序");
@@ -115,7 +141,7 @@ namespace OSUDesktop
 
             if (winseven)
             {
-                //如果Win7则选定Progman的Program Manager（貌似不可行？）
+                // 如果Win7则选定Progman的Program Manager（貌似不可行？）
                 workerw = FindWindow("Progman", null);
             }
             else
@@ -139,23 +165,109 @@ namespace OSUDesktop
             return workerw;
         }
 
+        private List<int> GetPIDByPName(string processName)
+        {
+            List<int> list = new List<int>();
+            using (ManagementObjectSearcher managementObjectSearcher = new ManagementObjectSearcher(string.Format("select Handle from Win32_Process where Name='{0}'", processName)))
+            {
+                using (ManagementObjectCollection managementObjectCollection = managementObjectSearcher.Get())
+                {
+                    foreach (ManagementBaseObject managementBaseObject in managementObjectCollection)
+                    {
+                        ManagementObject managementObject = (ManagementObject)managementBaseObject;
+                        int pid;
+                        Int32.TryParse(managementObject["Handle"].ToString(), out pid);
+                        list.Add(pid);
+                    }
+                }
+            }
+            return list;
+            // Console.WriteLine(list[0]);
+        }
+
+        public static IntPtr GetMainWindowHandle(int processId)
+        {
+            IntPtr MainWindowHandle = IntPtr.Zero;
+            EnumWindows(new EnumWindowsProc((hWnd, lParam) =>
+            {
+                IntPtr PID;
+                GetWindowThreadProcessId(hWnd, out PID);
+                Console.WriteLine("Pid: {0}, hWnd: {1}", PID, hWnd);
+                if (PID == lParam &&
+                    IsWindowVisible(hWnd) &&
+                    GetWindow(hWnd, GW_OWNER) == IntPtr.Zero)
+                {
+                    MainWindowHandle = hWnd;
+                    return false;
+                }
+                return true;
+            }), new IntPtr(processId));
+            Console.WriteLine("hWnd is " + MainWindowHandle.ToString() + " in " + processId.ToString());
+            return MainWindowHandle;
+        }
+
+        private static void KillProcessAndChildren(int pid)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher
+              ("Select * From Win32_Process Where ParentProcessID=" + pid);
+            ManagementObjectCollection moc = searcher.Get();
+            foreach (ManagementObject mo in moc)
+            {
+                KillProcessAndChildren(Convert.ToInt32(mo["ProcessID"]));
+            }
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                proc.Kill();
+            }
+            catch (ArgumentException)
+            {
+                // Process already exited.
+            }
+        }
+
         private void AsWallpaperToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            SetParent(p.MainWindowHandle, WorkerWIntPtr);//设置父窗体为桌面（注入WorkerW）
+            SetParent(p.MainWindowHandle, WorkerWIntPtr); // 设置父窗体为桌面（注入WorkerW）
         }
 
         public const int WM_CLOSE = 0x10;
-        //ExitWindows函数
+        // ExitWindows函数
 
         private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            SendMessage(p.MainWindowHandle, WM_CLOSE, 0, 0);//先关闭OSU!
-            Application.Exit();//然后关闭辅助程序
+            SendMessage(p.MainWindowHandle, WM_CLOSE, 0, 0); // 先关闭OSU!
+            Application.Exit(); // 然后关闭辅助程序
+            // GetMainWindowHandle(ModeA_PID);
         }
 
         private void ReShowOSUToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            SetParent(p.MainWindowHandle, IntPtr.Zero);//重置OSU!的父窗体为零指针，使其重新独立
+            SetParent(p.MainWindowHandle, IntPtr.Zero); // 重置OSU!的父窗体为零指针，使其重新独立
+        }
+
+        private void StartCheckBtn_Click(object sender, EventArgs e)
+        {
+            ModeA_Name = progNameBox.Text;
+            progNameBox.Enabled = false;
+            StartCheckBtn.Enabled = false;
+            StartCheckBtn.Text = "正在等待游戏启动";
+            StartOSU.Enabled = false;
+            checkTimer.Enabled = true;
+        }
+
+        private void CheckTimer_Tick(object sender, EventArgs e)
+        {
+            if (GetPIDByPName("MuseDash.exe").Count > 0)
+            {
+                checkTimer.Enabled = false;
+                MessageBox.Show("请先将游戏设置为无边框窗口全屏，然后在系统托盘设置壁纸即可~");
+                cmode = StartMode.ModeA;
+                ModeA_PID = GetPIDByPName(ModeA_Name)[0];
+                p = Process.GetProcessById(ModeA_PID);
+                Hide(); // 隐藏窗口
+                notifyIcon1.Visible = true; // 显示托盘图标
+            }
         }
     }
 }
